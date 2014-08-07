6b13dccaacd226ab42201d92a7a5990be36214f7
Apoorva Dornadula <adornadula@Apoorva-Dornadula.local>
2014-08-07 14:16:32 -0700

    Add & Edit preliminary functionalities done

--- companyContacts/companyContacts.py
+++ companyContacts/companyContacts.py
@@ -8,6 +8,8 @@ from contextlib import closing
 import smtplib
 from xlrd import open_workbook
 
+################### Variables ######################
+
 DATABASE = 'cc.db'
 DEBUG = True
 SECRET_KEY = 'development key'
@@ -16,6 +18,8 @@ USERNAME = 'admin'
 PASSWORD = 'default'
 
 workbook = open_workbook('/Users/adornadula/Perforce/ApoorvaPerforceWorkspace/ficds/OverallScan_detailed_report.xls')
+
+editResults = [];
 
 #creating the actual application
 app = Flask(__name__)
@@ -64,7 +68,7 @@ def add():
 
 @app.route('/edit')
 def edit():
-	return render_template('edit.html')
+	return render_template('edit.html', editResults = editResults)
 
 @app.route('/email')
 def email():
@@ -91,6 +95,56 @@ def update():
 			g.db.execute('insert into Company (Name, ContactName, ContactEmail, ContactPhoneNumber, Description) values (?, ?, ?, ?, ?)', [companyName, contactName, contactEmail, contactPhone, notes])
 			g.db.commit()
 	return redirect(url_for('view'))
+
+#Shows the edit form for the company
+@app.route('/editComp/<cID>')
+def editComp(cID):
+	global editResults
+	current = g.db.execute('SELECT Name, ContactName, ContactEmail, ContactPhoneNumber, Description, CompanyID FROM Company WHERE CompanyID=?', [cID])
+	editResult = [dict(Name=row[0], ContactName=row[1], ContactEmail=row[2], ContactPhoneNumber=row[3], Description=row[4], CompanyID=row[5]) for row in current.fetchall()]
+	editResults = []
+	return render_template("editCompany.html", editResults = editResult[0])
+
+
+################ Action ################
+@app.route('/addAction', methods=['POST'])
+def addAction():
+	companyName = request.form["compName"]
+	contactName = request.form["contName"]
+	contactEmail = request.form["contEmail"]
+	contactPhone = request.form["contPhone"]
+	notes = request.form["description"]
+
+	g.db.execute('insert into Company (Name, ContactName, ContactEmail, ContactPhoneNumber, Description) values (?, ?, ?, ?, ?)', [companyName, contactName, contactEmail, contactPhone, notes])
+	g.db.commit()
+
+	currentID = g.db.execute('SELECT max(CompanyID) FROM Company')
+	print(currentID.fetchall()[0][0])
+
+	return redirect(url_for('add'))
+
+#Displays the companies that fit the query
+@app.route('/editAction', methods= ['POST'])
+def editAction():
+	global editResults
+	companyName = request.form['compName']
+	current = g.db.execute('SELECT Name, ContactName, ContactEmail, ContactPhoneNumber, Description, CompanyID FROM Company WHERE Name=?', [companyName])
+	editResults = [dict(Name=row[0], ContactName=row[1], ContactEmail=row[2], ContactPhoneNumber=row[3], Description=row[4], CompanyID=row[5]) for row in current.fetchall()]
+	return redirect(url_for('edit'))
+
+#Updates the company in the database
+@app.route('/saveEdit/<cID>', methods=['POST'])
+def saveEdit(cID):
+	companyName = request.form["compName"]
+	contactName = request.form["contName"]
+	contactEmail = request.form["contEmail"]
+	contactPhone = request.form["contPhone"]
+	notes = request.form["description"]
+
+	g.db.execute('UPDATE Company SET Name=?, ContactName=?, ContactEmail=?, ContactPhoneNumber=?, Description=? WHERE CompanyID=?', [companyName, contactName, contactEmail, contactPhone, notes, cID])
+	g.db.commit()
+	return redirect(url_for('edit'))
+
 
 ################ Other ################
 
